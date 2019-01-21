83d6048f4bcf331abc8cc4ce917d6cc3d06588cc
PabloSz <pablosaez1995@gmail.com>
2019-01-21 17:51:56 -0300

    pose

--- src/components/HomepageHeading.js
+++ src/components/HomepageHeading.js
@@ -16,33 +16,67 @@ import {
   Image,
   Dropdown
 } from "semantic-ui-react";
+import sample from "../dist/images/google.png";
+import posed from "react-pose";
 
-const HomepageHeading = ({ mobile }) => (
-  <Container text>
-    <Header
-      as="h1"
-      content="Sinantica"
-      style={{
-        fontSize: mobile ? "2em" : "4em",
-        fontWeight: "normal",
-        marginBottom: 0,
-        marginTop: mobile ? "1.5em" : "3em"
-      }}
-    />
-    <Header
-      as="h2"
-      content="Somos Sinántica. Hacemos cosas sorprendentes."
-      style={{
-        fontSize: mobile ? "1.5em" : "1.7em",
-        fontWeight: "normal",
-        marginTop: mobile ? "0.5em" : "1.5em"
-      }}
-    />
-    {/* {<Button primary size="huge">
-        Get Started
-        <Icon name="right arrow" />
-      </Button>} */}
-  </Container>
-);
+const Img = posed.img({
+  nada: {
+    filter: "blur(0px)",
+    transition: {
+      duration: 2500
+    }
+  },
+  harto: {
+    filter: "blur(10px)",
+    transition: {
+      duration: 2500
+    }
+  }
+});
+class HomepageHeading extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      initialPose: "nada",
+      pose: "harto"
+    };
+  }
+
+  render() {
+    const { initialPose, pose } = this.state;
+    return (
+      <Container text>
+        <Header
+          as="h1"
+          content="Sinantica"
+          style={{
+            fontSize: "4em",
+            fontWeight: "normal",
+            marginBottom: 0,
+            marginTop: "3em"
+          }}
+        />
+        <Img
+          src={sample}
+          initialPose={initialPose}
+          pose={pose}
+          onPoseComplete={() =>
+            this.setState({ initialPose: pose, pose: initialPose })
+          }
+        />
+        <Header
+          as="h2"
+          content="Somos Sinántica. Hacemos cosas sorprendentes."
+          style={{
+            fontSize: "1.5em",
+            fontWeight: "normal",
+            marginTop: "1.5em"
+          }}
+        />
+      </Container>
+    );
+  }
+}
 
 export default HomepageHeading;
