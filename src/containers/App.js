df8a1c39c468ff0a87e759dbf386ec0585b5ea6f
Unknown <nachooo.j@live.com>
2019-01-23 13:21:45 -0300

    interfaz

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { DesktopContainer } from "../components";
-import { Landing, News } from "./";
+import { Landing, News, AboutUs } from "./";
+import { Container, Label, Icon } from "semantic-ui-react";
 class Who extends Component {
   render() {
     return <div>xd</div>;
   }
 }
+class Inv extends Component {
+  render() {
+    return (
+      <Container>
+        <Label>
+          <Icon name="mail" /> 23
+        </Label>
+      </Container>
+    );
+  }
+}
+
 export default class App extends Component {
   render() {
     return (
@@ -14,8 +27,8 @@ export default class App extends Component {
         <Switch>
           <DesktopContainer>
             <Route exact path="/" component={Landing} />
-            <Route exact path="/quienes_somos" component={Who} />
-            <Route exact path="/investigacion" component={Who} />
+            <Route exact path="/quienes_somos" component={AboutUs} />
+            <Route exact path="/investigacion" component={Inv} />
             <Route exact path="/noticias" component={News} />
             <Route exact path="/contacto" component={Who} />
           </DesktopContainer>
