8c237687fb747978357f13bb47a7215c93c84813
PabloSz <pablosaez1995@gmail.com>
2019-01-24 16:34:39 -0300

    mobile menu fix and menu active by route

--- src/components/DesktopContainer.js
+++ src/components/DesktopContainer.js
@@ -1,102 +1,94 @@
 import React, { Component } from "react";
-import {
-  Button,
-  Container,
-  Divider,
-  Grid,
-  Header,
-  Icon,
-  List,
-  Menu,
-  Responsive,
-  Segment,
-  Sidebar,
-  Visibility,
-  Tab,
-  Image,
-  Dropdown
-} from "semantic-ui-react";
-import { Link } from "react-router-dom";
-import { HomepageHeading, SampleImage } from "./";
-import { getWidth } from "../utils";
+import { Link, withRouter } from "react-router-dom";
+import { Container, Menu, Responsive } from "semantic-ui-react";
 import logo_small from "../dist/images/logo-small.png";
+import { getWidth } from "../utils";
 
-export default class DesktopContainer extends Component {
-  state = {
-    active: "inicio"
-  };
+export default withRouter(
+  class DesktopContainer extends Component {
+    constructor(props) {
+      super(props);
 
-  hideFixedMenu = () => this.setState({ fixed: false });
-  showFixedMenu = () => this.setState({ fixed: true });
+      this.state = {
+        active: this.props.location.pathname.slice(1),
+      };
+    }
 
-  render() {
-    const { children } = this.props;
-    const { fixed, active } = this.state;
+    hideFixedMenu = () => this.setState({ fixed: false });
+    showFixedMenu = () => this.setState({ fixed: true });
 
-    return (
-      <Responsive getWidth={getWidth} minWidth={Responsive.onlyTablet.minWidth}>
-        <Menu
-          fixed="top"
-          pointing={!fixed}
-          secondary={!fixed}
-          size="large"
-          style={{ backgroundColor: "white" }}
+    render() {
+      const { children } = this.props;
+      const { fixed, active } = this.state;
+
+      return (
+        <Responsive
+          getWidth={getWidth}
+          minWidth={Responsive.onlyTablet.minWidth}
         >
-          <Container>
-            <Menu.Item>
-              <img src={logo_small} size="mini" />
-            </Menu.Item>
-            <Menu.Item
-              header
-              as={Link}
-              to="/"
-              active={active === "inicio"}
-              onClick={() => this.setState({ active: "inicio" })}
-            >
-              Inicio
-            </Menu.Item>
-            <Menu.Item
-              header
-              as={Link}
-              to="/quienes_somos"
-              active={active === "quienes_somos"}
-              onClick={() => this.setState({ active: "quienes_somos" })}
-            >
-              Quienes Somos
-            </Menu.Item>
+          <Menu
+            fixed="top"
+            pointing={!fixed}
+            secondary={!fixed}
+            size="large"
+            style={{ backgroundColor: "white" }}
+          >
+            <Container>
+              <Menu.Item>
+                <img src={logo_small} size="mini" />
+              </Menu.Item>
+              <Menu.Item
+                header
+                as={Link}
+                to="/"
+                active={active === ""}
+                onClick={() => this.setState({ active: "" })}
+              >
+                Inicio
+              </Menu.Item>
+              <Menu.Item
+                header
+                as={Link}
+                to="/quienes_somos"
+                active={active === "quienes_somos"}
+                onClick={() => this.setState({ active: "quienes_somos" })}
+              >
+                Quienes Somos
+              </Menu.Item>
 
-            <Menu.Item
-              header
-              as={Link}
-              to="/investigacion"
-              active={active === "investigacion"}
-              onClick={() => this.setState({ active: "investigacion" })}
-            >
-              Investigación
-            </Menu.Item>
-            <Menu.Item
-              header
-              as={Link}
-              to="/noticias"
-              active={active === "noticias"}
-              onClick={() => this.setState({ active: "noticias" })}
-            >
-              Noticias
-            </Menu.Item>
-            <Menu.Item
-              header
-              as={Link}
-              to="/contacto"
-              active={active === "contacto"}
-              onClick={() => this.setState({ active: "contacto" })}
-            >
-              Contacto
-            </Menu.Item>
-            <Menu.Item position="right" />
-          </Container>
-        </Menu>
-        {children}
-      </Responsive>
-    );
+              <Menu.Item
+                header
+                as={Link}
+                to="/investigacion"
+                active={active === "investigacion"}
+                onClick={() => this.setState({ active: "investigacion" })}
+              >
+                Investigación
+              </Menu.Item>
+              <Menu.Item
+                header
+                as={Link}
+                to="/noticias"
+                active={active === "noticias"}
+                onClick={() => this.setState({ active: "noticias" })}
+              >
+                Noticias
+              </Menu.Item>
+              <Menu.Item
+                header
+                as={Link}
+                to="/contacto"
+                active={active === "contacto"}
+                onClick={() => this.setState({ active: "contacto" })}
+              >
+                Contacto
+              </Menu.Item>
+              <Menu.Item position="right" />
+            </Container>
+          </Menu>
+          {children}
+        </Responsive>
+      );
+    }
   }
-}
+);
--- src/components/MobileContainer.js
+++ src/components/MobileContainer.js
@@ -1,81 +1,124 @@
 import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
 import {
-  Button,
   Container,
-  Divider,
-  Grid,
-  Header,
   Icon,
-  List,
   Menu,
   Responsive,
   Segment,
   Sidebar,
-  Visibility,
-  Tab,
-  Image,
-  Dropdown
 } from "semantic-ui-react";
-import { HomepageHeading } from "./";
 import { getWidth } from "../utils";
 
-export default class MobileContainer extends Component {
-  state = {};
+export default withRouter(
+  class MobileContainer extends Component {
+    constructor(props) {
+      super(props);
 
-  handleSidebarHide = () => this.setState({ sidebarOpened: false });
+      this.state = {
+        active: this.props.location.pathname.slice(1),
+        sidebarOpened: false,
+      };
+    }
 
-  handleToggle = () => this.setState({ sidebarOpened: true });
+    handleSidebarHide = () => this.setState({ sidebarOpened: false });
 
-  render() {
-    const { children } = this.props;
-    const { sidebarOpened } = this.state;
+    handleToggle = () => this.setState({ sidebarOpened: true });
 
-    return (
-      <Responsive
-        as={Sidebar.Pushable}
-        getWidth={getWidth}
-        maxWidth={Responsive.onlyMobile.maxWidth}
-      >
-        <Sidebar
-          as={Menu}
-          animation="push"
-          onHide={this.handleSidebarHide}
-          vertical
-          visible={sidebarOpened}
-          style={{
-            marginBottom: 0
-          }}
+    render() {
+      const { children } = this.props;
+      const { sidebarOpened, active } = this.state;
+
+      return (
+        <Responsive
+          as={Sidebar.Pushable}
+          getWidth={getWidth}
+          maxWidth={Responsive.onlyMobile.maxWidth}
         >
-          <Menu.Item as="a" active>
-            Inicio
-          </Menu.Item>
-          <Menu.Item as="a">Quienes Somos</Menu.Item>
-          <Menu.Item as="a">Investigación</Menu.Item>
-          <Menu.Item as="a">Noticias</Menu.Item>
-          <Menu.Item as="a">Contacto</Menu.Item>
-        </Sidebar>
-
-        <Sidebar.Pusher dimmed={sidebarOpened}>
-          <Segment
-            textAlign="center"
+          <Sidebar
+            as={Menu}
+            animation="push"
+            onHide={this.handleSidebarHide}
             vertical
+            visible={sidebarOpened}
             style={{
-              margin: 0,
-              padding: "0.1em"
+              marginBottom: 0,
             }}
           >
-            <Container>
-              <Menu pointing secondary size="large">
-                <Menu.Item onClick={this.handleToggle}>
-                  <Icon name="sidebar" />
-                </Menu.Item>
-              </Menu>
-            </Container>
-          </Segment>
+            <Menu.Item
+              as={Link}
+              to="/"
+              active={active === ""}
+              onClick={() =>
+                this.setState({ active: "", sidebarOpened: false })
+              }
+            >
+              Inicio
+            </Menu.Item>
+            <Menu.Item
+              as={Link}
+              to="/quienes_somos"
+              active={active === "quienes_somos"}
+              onClick={() =>
+                this.setState({ active: "quienes_somos", sidebarOpened: false })
+              }
+            >
+              Quienes Somos
+            </Menu.Item>
+            <Menu.Item
+              as={Link}
+              to="/investigacion"
+              active={active === "investigacion"}
+              onClick={() =>
+                this.setState({ active: "investigacion", sidebarOpened: false })
+              }
+            >
+              Investigación
+            </Menu.Item>
+            <Menu.Item
+              as={Link}
+              to="/noticias"
+              active={active === "noticias"}
+              onClick={() =>
+                this.setState({ active: "noticias", sidebarOpened: false })
+              }
+            >
+              Noticias
+            </Menu.Item>
+            <Menu.Item
+              as={Link}
+              to="/contacto"
+              active={active === "contacto"}
+              onClick={() =>
+                this.setState({ active: "contacto", sidebarOpened: false })
+              }
+            >
+              Contacto
+            </Menu.Item>
+          </Sidebar>
 
-          {children}
-        </Sidebar.Pusher>
-      </Responsive>
-    );
+          <Sidebar.Pusher dimmed={sidebarOpened}>
+            <Segment
+              textAlign="center"
+              vertical
+              style={{
+                margin: 0,
+                padding: "0.1em",
+              }}
+            >
+              <Container>
+                <Menu pointing secondary size="large">
+                  <Menu.Item onClick={this.handleToggle}>
+                    <Icon name="sidebar" />
+                  </Menu.Item>
+                </Menu>
+              </Container>
+            </Segment>
+
+            {children}
+          </Sidebar.Pusher>
+        </Responsive>
+      );
+    }
   }
-}
+);
