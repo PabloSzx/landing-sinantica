f413e1852296365267be5de1cad03636d8f91636
jelbo96 <nachooo.j@live.com>
2019-01-24 18:27:14 -0300

    subcomponent templates (no refactor)

--- src/containers/AboutUs.js
+++ src/containers/AboutUs.js
@@ -9,7 +9,9 @@ import {
   Segment
 } from "semantic-ui-react";
 import sample from "../dist/images/google.png";
+import example from "../dist/images/sample.png";
 import { Footer } from "../components";
+import { relative } from "path";
 const DIV = styled.div`
   position: relative;
   background-color: white;
@@ -17,6 +19,25 @@ const DIV = styled.div`
   width: 45rem;
   top: 10.5em;
   left: 10rem;
+`;
+
+const DIVBox = styled.div`
+  position: absolute;
+  background-color: white;
+  textalign: center;
+  height: 50vh;
+  width: 50vw;
+  top: 10vh;
+  left: 25vw;
+`;
+
+const DIVImage = styled.div`
+  position: absolute;
+  backgroundcolor: red;
+  maxheight: 50vh;
+  width: 50vw;
+  top: 10vh;
+  left: 10vw;
 `;
 const Title = styled.h2`
   font-size: 1.5em;
@@ -80,22 +101,161 @@ export default class AboutUs extends Component {
             />
           </div>
         </Segment>
+
+        {/* 2 Imagenes + texto*/}
+        <Segment style={{ padding: "0em 0em" }} vertical>
+          <img
+            src={example}
+            style={{
+              zIndex: "-1",
+              position: "relative",
+              width: "48.9vw",
+              marginTop: 0
+            }}
+          />
+          <img
+            src={example}
+            style={{
+              zIndex: "-1",
+              position: "relative",
+              float: "right",
+              width: "48.9vw",
+              marginTop: 0
+            }}
+          />
+
+          <DIVBox>
+            <Title>LOREM IPSUM DOLOR</Title>
+            <P style={{ top: "0em " }}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              sagittis placerat mollis. Ut ac eleifend ligula. Aenean eget
+              cursus ex. Integer et lorem blandit, dignissim metus sed, suscipit
+              quam. Nulla placerat velit quis vulputate commodo. Quisque gravida
+              pellentesque ante ac tempus. Curabitur sodales vel purus eget
+              porttitor. Cras lacus diam, tempus ut dolor sit amet, bibendum
+              ornare arcu.
+            </P>
+          </DIVBox>
+        </Segment>
+
+        {/* Solo Texto*/}
+
         <Segment style={{ padding: "8em 0em" }} vertical>
           <Container text>
-            <Header as="h3" className="title-font" style={{ fontSize: "5em" }}>
-              Quienes Somos
-            </Header>
             <p style={{ fontSize: "1.33em" }}>
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-              commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque
-              penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              sagittis placerat mollis. Ut ac eleifend ligula. Aenean eget
+              cursus ex. Integer et lorem blandit, dignissim metus sed, suscipit
+              quam. Nulla placerat velit quis vulputate commodo. Quisque gravida
+              pellentesque ante ac tempus. Curabitur sodales vel purus eget
+              porttitor. Cras lacus diam, tempus ut dolor sit amet, bibendum
+              ornare arcu. Cras nisl dui, imperdiet a venenatis eu, aliquet in
+              erat. Ut accumsan suscipit dignissim. Ut non odio lorem.
+              Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+              posuere cubilia Curae; Nunc blandit libero vitae lorem elementum
+              suscipit. Praesent porttitor nibh diam, iaculis aliquam metus
+              bibendum eu.
             </p>
-            <Button icon labelPosition="right" floated="right">
-              Ver más
-              <Icon name="right arrow" />
-            </Button>
           </Container>
         </Segment>
+
+        {/* Texto en imagen*/}
+        <Segment style={{ padding: "0em 0em" }} vertical>
+          <img
+            src={example}
+            style={{
+              zIndex: "-1",
+              position: "relative",
+              width: "100vw",
+              objectFit: "cover",
+              maxHeight: "50vh",
+              marginTop: 0
+            }}
+          />
+          <DIVImage>
+            <Title style={{ color: "white" }}>LOREM IPSUM DOLOR</Title>
+            <P style={{ top: "-2em ", left: "-0.3em", color: "white" }}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              sagittis placerat mollis. Ut ac eleifend ligula. Aenean eget
+              cursus ex. Integer et lorem blandit, dignissim metus sed, suscipit
+              quam. Nulla placerat velit quis vulputate commodo.
+            </P>
+          </DIVImage>
+        </Segment>
+
+        {/* 3 Imagenes + texto*/}
+        <Segment style={{ padding: "0em 0em" }} vertical>
+          <img
+            src={example}
+            style={{
+              zIndex: "-1",
+              position: "relative",
+              objectFit: "cover",
+              width: "32.9vw",
+              float: "left",
+              height: "70vh",
+              marginTop: 0
+            }}
+          />
+          <img
+            src={example}
+            style={{
+              zIndex: "-1",
+              position: "relative",
+              objectFit: "cover",
+              float: "right",
+              width: "32.9vw",
+              height: "70vh",
+              marginTop: 0
+            }}
+          />
+          <img
+            src={example}
+            style={{
+              zIndex: "-1",
+
+              objectFit: "cover",
+              display: "block",
+              marginLeft: "auto",
+              marginRight: "auto",
+              width: "32.5vw",
+              height: "70vh",
+              marginTop: 0
+            }}
+          />
+
+          <DIVBox>
+            <Title>LOREM IPSUM DOLOR</Title>
+            <P style={{ top: "0em " }}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              sagittis placerat mollis. Ut ac eleifend ligula. Aenean eget
+              cursus ex. Integer et lorem blandit, dignissim metus sed, suscipit
+              quam. Nulla placerat velit quis vulputate commodo. Quisque gravida
+              pellentesque ante ac tempus. Curabitur sodales vel purus eget
+              porttitor. Cras lacus diam, tempus ut dolor sit amet, bibendum
+              ornare arcu.
+            </P>
+          </DIVBox>
+        </Segment>
+        <Segment style={{ padding: "8em 0em" }} vertical>
+          <Container text>
+            <p style={{ fontSize: "1.33em" }}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              sagittis placerat mollis. Ut ac eleifend ligula. Aenean eget
+              cursus ex. Integer et lorem blandit, dignissim metus sed, suscipit
+              quam. Nulla placerat velit quis vulputate commodo. Quisque gravida
+              pellentesque ante ac tempus. Curabitur sodales vel purus eget
+              porttitor. Cras lacus diam, tempus ut dolor sit amet, bibendum
+              ornare arcu. Cras nisl dui, imperdiet a venenatis eu, aliquet in
+              erat. Ut accumsan suscipit dignissim. Ut non odio lorem.
+              Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+              posuere cubilia Curae; Nunc blandit libero vitae lorem elementum
+              suscipit. Praesent porttitor nibh diam, iaculis aliquam metus
+              bibendum eu.
+            </p>
+          </Container>
+        </Segment>
+
         <Footer />
       </Fragment>
     );
